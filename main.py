3020af2bfca27f77c8ebfee802b248d3673c11f2
Ítalo Leal <italoleallanasantos@gmail.com>
2025-11-18 01:23:28 -0300

    Refactor main.py to include data preprocessing, model training, and evaluation steps and refactor ghost table

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-import scikit
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
-#Preprocessing
+print("# All the packages were imported with success!")
 
-#Dimensionality reduction
+def main():
 
-#Regression
+    table_path: str = "files\\tabela_ghost.csv"
+    df: pd.DataFrame = pd.read_csv(table_path, sep=";")
 
-# Tamanho do CSV Tratado:
-# 9 Colunas x 416955 Linhas
+    # Criando variável-alvo com shift de 1 mês
+    df["future_cases"] = df.groupby("municipality_code_ibge")["total_cases"].shift(-1)
+    df = df.dropna() # Remove missing values (NaN or None) from the DataFrame.
 
-# Missão: tentar prever Dezembro / o ano de 2026
+    X = df[[
+        'rainfall_mm',
+        'average_humidity',
+        'average_temperature',
+        'dengue_cases',
+        'chikungunya_cases',
+        'zika_cases',
+        'total_cases',
+        'estimated_population',
+        ]]    
+
+    y = df['future_cases']
+
+    # pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    print(df)
+    print(X)
+    print(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    modelo = RandomForestRegressor(n_estimators=300)
+    modelo.fit(X_train, y_train)
+
+    previsoes = modelo.predict(X_test)
+    erro = mean_absolute_error(y_test, previsoes)
+
+    print("MAE:", erro)
+
+    print("# The program was executed successfully!")
+
+
 
 if __name__ == "__main__":
-    print("Opa")
+    main()
+    # Missão: tentar prever Dezembro / o ano de 2026
+    
 
