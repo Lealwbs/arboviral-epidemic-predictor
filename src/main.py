bb8735fa3ba22c6b2e8e6beb6aedb6d3c3084daf
Ítalo Leal <italoleallanasantos@gmail.com>
2025-11-18 21:13:09 -0300

    Backup predictor / main not working

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from predictor import Predictor
+from alerts import Alert
+
+if __name__ == "__main__":
+    table: str = ".\\files\\tabela_ghost.csv"
+    P = Predictor(tablepath=table)
+    main()
+    # Missão: tentar prever Dezembro
+    
+
---
+++ src/predictor.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+class Predictor:
+    def __init__(self, tablepath: str) -> None:
+        self.tablepath = tablepath
+
+    def load_data(self) -> None:
+        df: pd.DataFrame = pd.read_csv(self.tablepath, sep=";")
+        X = df[[
+            'dengue_cases',
+            'estimated_population',
+            'rainfall_mm',
+            'average_humidity',
+            'average_temperature',
+            ]] 
+
+    
+    def ababa():
+     
+        
+
+        # Criando variável-alvo com shift de 1 mês
+        df["future_cases"] = df.groupby("municipality_code_ibge")["total_cases"].shift(-1)
+        df = df.dropna() # Remove missing values (NaN or None) from the DataFrame.
+
+   
+
+        y = df['future_cases']
+
+        # pd.set_option('display.max_columns', None)
+        pd.set_option('display.max_rows', None)
+        # print(df)
+        # print(X)
+        # print(y)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        #print(X_train, X_test, y_train, y_test)
+
+        modelo = RandomForestRegressor(n_estimators=300)
+        modelo.fit(X_train, y_train)
+
+        previsoes = modelo.predict(X_test)
+        # print(previsoes)
+        erro = mean_absolute_error(y_test, previsoes)
+
+        print("Mean Absolute Error:", erro)
+
+        print()
+        print("#" * 48)
+        print("# The program was executed successfully!       #")
+        print("#" * 48)
+
+        data: dict[str:str] = {} 
+        print(get_alert(data))
+
+
+
