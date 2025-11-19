811a0d2d85970459f59f768b486ca95e0b5fc60e
Ítalo Leal <italoleallanasantos@gmail.com>
2025-11-19 01:09:35 -0300

    Refactor main execution flow and update Predictor class for city code validation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from predictor import Predictor
 from alerts import Alert
 
+
 if __name__ == "__main__":
-    table: str = ".\\files\\tabela_ghost.csv"
-    P = Predictor(tablepath=table)
-    main()
-    # Missão: tentar prever Dezembro
+
+    GHOST_TABLE: str = ".\\data\\ghost_table.csv"
+    MASTER_TABLE: str = ".\\data\\master_table.csv"
     
+    P = Predictor(tablepath=MASTER_TABLE)
+
+    A: Alert = P.predict_outbreak(city_code="3106200", year="2025", month="12")    # Belo Horizonte
+
+    print(A)
 
--- src/predictor.py
+++ src/predictor.py
@@ -1,62 +1,77 @@
-import pandas as pd
+from alerts import Alert # Local import
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
+from datetime import datetime
+import pandas as pd
+
+
+IBGE_CITY_CODES: dict[str:str] = {
+    "3106200": "Belo Horizonte",
+    "3304557": "Rio de Janeiro",
+    "3509502": "São Paulo",
+    "2927408": "Salvador",  
+    "5300108": "Brasília",
+    }
+
 
 class Predictor:
     def __init__(self, tablepath: str) -> None:
         self.tablepath = tablepath
 
-    def load_data(self) -> None:
-        df: pd.DataFrame = pd.read_csv(self.tablepath, sep=";")
-        X = df[[
+
+    def _load_data(self, city_code: str) -> pd.DataFrame:
+        if city_code not in IBGE_CITY_CODES:
+            raise ValueError("City code not found in IBGE city codes.")
+        
+        result: pd.DataFrame = pd.read_csv(self.tablepath, sep=";")
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', None)
+
+        return result[result["municipality_code_ibge"] == int(city_code)]
+
+    
+    def predict_outbreak(self, city_code: str, year: str, month: str) -> Alert:
+        if int(year) < datetime.now().year or (int(year) == datetime.now().year and int(month) < datetime.now().month):
+            raise ValueError("Cannot predict for past dates.")
+        
+        df = self._load_data(city_code)
+        X_COMPONENTS = df[[
             'dengue_cases',
             'estimated_population',
             'rainfall_mm',
             'average_humidity',
             'average_temperature',
             ]] 
-
-    
-    def ababa():
-     
         
-
-        # Criando variável-alvo com shift de 1 mês
-        df["future_cases"] = df.groupby("municipality_code_ibge")["total_cases"].shift(-1)
-        df = df.dropna() # Remove missing values (NaN or None) from the DataFrame.
-
-   
-
-        y = df['future_cases']
-
-        # pd.set_option('display.max_columns', None)
-        pd.set_option('display.max_rows', None)
         # print(df)
-        # print(X)
-        # print(y)
+        
+        result: Alert = Alert(
+            # event="Dengue",
+            # severity="Alto Risco",
+            # certainly="100%",
+            # year="2024",
+            # month="06",
+            # city_name="Belo Horizonte",
+            # city_code="3106200"
+        )
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        return result
 
-        #print(X_train, X_test, y_train, y_test)
+    #     # Criando variável-alvo com shift de 1 mês
+    #     df["future_cases"] = df.groupby("municipality_code_ibge")["total_cases"].shift(-1)
+    #     df = df.dropna() # Remove missing values (NaN or None) from the DataFrame.
+   
+    #     y = df['future_cases']
 
-        modelo = RandomForestRegressor(n_estimators=300)
-        modelo.fit(X_train, y_train)
-
-        previsoes = modelo.predict(X_test)
-        # print(previsoes)
-        erro = mean_absolute_error(y_test, previsoes)
-
-        print("Mean Absolute Error:", erro)
-
-        print()
-        print("#" * 48)
-        print("# The program was executed successfully!       #")
-        print("#" * 48)
-
-        data: dict[str:str] = {} 
-        print(get_alert(data))
+    #     X_train, X_test, y_train, y_test = train_test_split(X_COMPONENTS, y, test_size=0.2, random_state=42)
 
 
+    #     modelo = RandomForestRegressor(n_estimators=300)
+    #     modelo.fit(X_train, y_train)
 
+    #     previsoes = modelo.predict(X_test)
+    #     erro = mean_absolute_error(y_test, previsoes)
+
+    #     print("Mean Absolute Error:", erro)
